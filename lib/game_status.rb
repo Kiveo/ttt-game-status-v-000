b08831ed5c4db1d67402b98a28105a39ae6687a1
Maverick Tesini <mavericktesini@gmail.com>
2018-02-22 17:32:20 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -4,3 +4,13 @@ def position_taken?(board, index)
 end
 
 # Define your WIN_COMBINATIONS constant
+WIN_COMBINATIONS = [
+  [0,1,2] #1
+  [3,4,5] #2
+  [6,7,9] #3
+  [0,3,6] #4
+  [1,4,7] #5
+  [2,5,9] #6
+  [0,4,9] #7
+  [2,4,6] #8 possible win scenarios
+]
